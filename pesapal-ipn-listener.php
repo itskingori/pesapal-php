71951538e6d56bd4b681b179875901c720982b67
King'ori Maina <j@kingori.co>
2013-06-24 16:47:53 +0300

    Make pesapal-ipn-listener.php logic a bit more clearer (logic+code)

--- pesapal-ipn-listener.php
+++ pesapal-ipn-listener.php
@@ -15,7 +15,8 @@ $consumer_secret = 'YOUR_PESAPAL_MERCHANT_CONSUMER_SECRET';
 // to go live!
 $statusrequestAPI = 'https://demo.pesapal.com/api/QueryPaymentStatus';
 
-// Fetch parameters sent to you by PesaPal IPN call
+// Fetch parameters sent to you by PesaPal IPN call. PesaPal will call the URL
+// you entered above with the below query parameters;
 $pesapal_notification_type        = $_GET['pesapal_notification_type'];
 $pesapal_transaction_tracking_id  = $_GET['pesapal_transaction_tracking_id'];
 $pesapal_merchant_reference       = $_GET['pesapal_merchant_reference'];
@@ -58,14 +59,43 @@ if ($pesapal_notification_type == 'CHANGE' && $pesapal_transaction_tracking_id !
 
    // Transaction status
    $elements = preg_split('/=/',substr($response, $header_size));
-   $status = $elements[1];
+   $status = $elements[1]; // PENDING, COMPLETED or FAILED
 
    curl_close ($ch);
-   
-   // UPDATE YOUR DB TABLE WITH NEW STATUS FOR TRANSACTION WITH
-   // pesapal_transaction_tracking_id $pesapal_transaction_tracking_id
 
-   if (DB_UPDATE_IS_SUCCESSFUL) {
+    // At this point $status may have value of PENDING, COMPLETED or FAILED.
+    // Please note that (as mentioned above) PesaPal will call the URL you
+    // entered above with the 3 query parameters. You must respond to the HTTP
+    // request with the same data that you received from PesaPal. PesaPal will
+    // retry a number of times, if they don't receive the correct response (for
+    // example due to network failure). So if successful, we update our DB ...
+    // if it FAILED, we can cancel out transaction in our DB and notify user
+   if ($status = 'COMPLETED') {
+
+        // Transaction confirmed that it's successful so we update the DB
+
+        // UPDATE YOUR DB TABLE WITH NEW STATUS FOR TRANSACTION WITH
+        // pesapal_transaction_tracking_id $pesapal_transaction_tracking_id
+
+        $db_update_successful = IF_YOUR_DB_UPDATE_IS_SUCCESSFUL_THIS_IS_UPTO_YOU_TO_DETERMINE;
+
+        if ($db_update_successful) {
+
+            // Send Pesapal response only if we were able to update DB
+            $resp = 'pesapal_notification_type='.$pesapal_notification_type;
+            $resp .= '&pesapal_transaction_tracking_id='.$pesapal_transaction_tracking_id;
+            $resp .= '&pesapal_merchant_reference='.$pesapal_merchant_reference;
+            ob_start();
+            echo $resp;
+            ob_flush();
+       }
+    }
+    elseif ($status = 'FAILED') {
+
+        // Transaction confirmed that it's NOT successful so we update the DB
+
+        // UPDATE YOUR DB TABLE AND CANCEL TRANSACTION OR MARK AS FAILED, POSSIBLY NOTIFY USER
+        // pesapal_transaction_tracking_id $pesapal_transaction_tracking_id
 
         $resp = 'pesapal_notification_type='.$pesapal_notification_type;
         $resp .= '&pesapal_transaction_tracking_id='.$pesapal_transaction_tracking_id;
@@ -73,7 +103,11 @@ if ($pesapal_notification_type == 'CHANGE' && $pesapal_transaction_tracking_id !
         ob_start();
         echo $resp;
         ob_flush();
-        exit;
-   }
+    }
+
+    // If PENDING ... we do nothing ... Pesapal will keep sending us the IPN but
+    // at least we know it's pending
+
+    exit;
 }
 ?>
